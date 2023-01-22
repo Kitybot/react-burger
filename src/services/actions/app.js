e8e1179efab3bbf607faa9fd2d78765b55599e7f
Kitybot <nick34goalie@yandex.ru>
2023-01-22 22:01:22 +0300

    commit: исправил ошибку ревью

--- src/services/actions/app.js
+++ src/services/actions/app.js
@@ -1,5 +1,11 @@
 import { deleteIngrdientDetails } from './ingredient-detalis';
 import { resetConstructorActionCreator } from './burger-constructor';
+import { fetchUsersStarted } from './ingredient-detalis'
+import { fetchUsersSuccess } from './ingredient-detalis'
+import { fetchUsersFailure } from './ingredient-detalis'
+import { fetchStarted } from './order-detalis'
+import { fetchSuccess } from './order-detalis'
+import { fetchFailure } from './order-detalis'
 
 export const OPEN_MODAL = 'OPEN_MODAL';
 export const CLOSE_MODAL = 'CLOSE_MODAL';
--- src/services/actions/order-detalis.js
+++ src/services/actions/order-detalis.js
@@ -3,6 +3,29 @@ import { closeModal, openModalActionCreator } from '../actions/app';
 
 export const COUNT_PRICE_BURGER = 'COUNT_PRICE_BURGER';
 export const SAVE_ORDER_DATA = 'SAVE_ORDER_DATA';
+export const GET_ORDER_DETALIS__REQUEST = 'GET_ORDER_DETALIS__REQUEST';
+export const GET_ORDER_DETALIS__SUCCESS = 'GET_ORDER_DETALIS__SUCCESS';
+export const GET_ORDER_DETALIS__EROOR = 'GET_ORDER_DETALIS__EROOR';
+
+export const fetchUsersStarted = () => {
+  return {
+      type: GET_ORDER_DETALIS__REQUEST,
+  };
+};
+
+export const fetchUsersSuccess = users => {
+  return {
+      type: GET_ORDER_DETALIS__SUCCESS,
+      payload: users,
+  };
+};
+
+export const fetchUsersFailure = error => {
+  return {
+      type: GET_ORDER_DETALIS__EROOR,
+      payload: error,
+  };
+};
 
 export function sendOrder(setRequest, constructorIngredients) {
   return function(dispatch) {
