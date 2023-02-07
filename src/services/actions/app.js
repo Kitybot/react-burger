326e4d93127a64bcd31bc323d99286edf27cae96
Kitybot <nick34goalie@yandex.ru>
2023-02-07 16:28:22 +0300

    commit: создал новую ветку и переделал папки services

--- src/services/actions/app.js
+++ src/services/actions/app.js
@@ -9,6 +9,14 @@ import { fetchFailure } from './order-detalis'
 
 export const OPEN_MODAL = 'OPEN_MODAL';
 export const CLOSE_MODAL = 'CLOSE_MODAL';
+export const CHANGE_ACTIVE_PAGE = 'CHANGE_ACTIVE_PAGE';
+
+export function changeActivePageActionCreator(page) {
+  return {
+    type: CHANGE_ACTIVE_PAGE,
+    activePage: page
+  }
+}
 
 export function openModalActionCreator(typeModal, message = '') {
   return {
@@ -18,7 +26,7 @@ export function openModalActionCreator(typeModal, message = '') {
   }
 }
 
-export function closeModal(isModalActive) {
+export function closeModal(isModalActive, saveBurger) {
   return function(dispatch) {
     dispatch({
       type: CLOSE_MODAL,
@@ -28,7 +36,9 @@ export function closeModal(isModalActive) {
         dispatch(deleteIngrdientDetails());
         break;
       case 'orderDetails':
-        dispatch(resetConstructorActionCreator());
+        if (!saveBurger) {
+          dispatch(resetConstructorActionCreator());
+        }
         break;
       default:
         return;
